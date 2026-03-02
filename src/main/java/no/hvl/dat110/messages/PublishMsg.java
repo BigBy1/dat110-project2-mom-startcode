9d0a125823892ec34e077d4bec4fd506477e62ae
BigBy1 <brage.brekkeflat@gmail.com>
2026-03-02 21:17:29 +0100

    Liten feil står igjen i oppgave A

--- src/main/java/no/hvl/dat110/messages/PublishMsg.java
+++ src/main/java/no/hvl/dat110/messages/PublishMsg.java
@@ -1,23 +1,52 @@
 package no.hvl.dat110.messages;
 
-import no.hvl.dat110.common.TODO;
-
 public class PublishMsg extends Message {
-	
+
 	// message sent from client to create publish a message on a topic 
+	
+	private String user = "";
+	private String topic = "";
+	private String message = "";
 
 	public PublishMsg(String user, String topic, String message) {
-
+		this.user = user;
+		this.topic = topic;
+		this.message = message;
 	}
 
+	public String getUser() {
+		return user;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	
 	// TODO:
 	// Implement object variables - a topic and a message is required
 
 	// Complete the constructor, get/set-methods, and toString method
 	// as described in the project text
-	
+
 	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	//string metoden
+	public String toString() {
+		return "Vi har motatt en forespørsel fra " + user + ", anngående " + topic + " med verdien "
+				+ message;
 	}
 }
